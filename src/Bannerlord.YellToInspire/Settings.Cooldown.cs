b4d19a0ef8fa3e6ccff9151c145c09acb1ed1261
Vitalii Mikhailov <personal@aragas.org>
2023-06-30 11:45:31 +0300

    * Changed the default Cooldown settings:
      * Base Ability Cooldown - 120,
      * Ability Cooldown Decrease per Level - 200, max 500
    * Added explicit v1.1.3-1.2.0 support

--- src/Bannerlord.YellToInspire/Settings.Cooldown.cs
+++ src/Bannerlord.YellToInspire/Settings.Cooldown.cs
@@ -7,10 +7,10 @@ namespace Bannerlord.YellToInspire
     {
         [SettingPropertyFloatingInteger("{=dpeaW5tpf0}Base Ability Cooldown", 0f, 300f, "#0 sec", Order = 0, RequireRestart = false, HintText = "{=E9nVaeVhWx}Determines the base length of the cooldown period in seconds between ability uses, also the value for Custom Battles.")]
         [SettingPropertyGroup("{=ZX1S3NcDvK}Cooldown Gameplay System", GroupOrder = 2)]
-        public float BaseAbilityCooldown { get; set; } = 10.0f;
+        public float BaseAbilityCooldown { get; set; } = 120.0f;
 
-        [SettingPropertyFloatingInteger("{=pVdQUEHQub}Ability Cooldown Decrease per Level", 0f, 200f, "#0 ms", Order = 1, RequireRestart = false, HintText = "{=dmGkKtAq4M}Determines how much less cooldown time in seconds per level in Leadership. Used in the Cooldown Gameplay Type.")]
+        [SettingPropertyFloatingInteger("{=pVdQUEHQub}Ability Cooldown Decrease per Level", 0f, 500f, "#0 ms", Order = 1, RequireRestart = false, HintText = "{=dmGkKtAq4M}Determines how much less cooldown time in seconds per level in Leadership. Used in the Cooldown Gameplay Type.")]
         [SettingPropertyGroup("{=ZX1S3NcDvK}Cooldown Gameplay System", GroupOrder = 2)]
-        public float AbilityCooldownDecreasePerLevel { get; set; } = 150f;
+        public float AbilityCooldownDecreasePerLevel { get; set; } = 200f;
     }
 }
